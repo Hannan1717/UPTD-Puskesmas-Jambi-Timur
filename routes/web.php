cc0353e8eb0bfa2f0276899bb8d3ef35cf035b4d
Hannan1717 <hananelang17@gmail.com>
2024-08-19 19:58:51 +0700

    [update] clear route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StuntingController;
-use App\Http\Controllers\VisitorController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Facades\Excel;
@@ -13,15 +12,13 @@ use Maatwebsite\Excel\Facades\Excel;
 // Define the pattern for slug
 Route::pattern('slug', '[a-zA-Z0-9\-]+');
 
-// Route::post('/article', [ArticleController::class, 'store'])->name('article.store');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/calculate', [HomeController::class, 'calculate'])->name('home.cal');
 Route::get('/article/{slug}', [ArticleController::class, 'showGuest'])->name('home.article.show');
 Route::get('/article/', [HomeController::class, 'articleHome'])->name('home.article');
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::prefix('admin')->middleware('auth')->group(function () {
-    // Route::get('/', [VisitorController::class, 'showCount'])->name('admin.dashboard');
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
     Route::resource('article', ArticleController::class)->parameters([
         'article' => 'slug',
